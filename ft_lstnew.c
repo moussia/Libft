1fc5c742c815de6e50e21de2dc71b58d14f158a6
Moussia MOTTAL <mmottal@e1r6p5.42.fr>
2018-03-14 13:47:23 +0100

    Voici la Libft que nous devions realiser au debut de notre cursus a 42.

---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmottal <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/27 11:20:13 by mmottal           #+#    #+#             */
+/*   Updated: 2017/11/27 12:12:40 by mmottal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	if (!(new = (t_list *)malloc(sizeof(*new))))
+		return (NULL);
+	if (content == NULL)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		if (!(new->content = malloc(content_size)))
+		{
+			free(new);
+			return (NULL);
+		}
+		ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
+	}
+	new->next = NULL;
+	return (new);
+}
